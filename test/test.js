66b535e099e85476e1ea2b5ce1f13606bd1b7e8f
Daniel Gasienica <daniel@gasienica.ch>
2011-04-17 23:45:53 -0700

    Rename test script for easier testing.

--- test/test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert'),
-	HttpStatus = require('../index');
-
-module.exports = {
-	'Test HTTP Status Code': function(){
-		assert.eql(200, HttpStatus.OK);
-		assert.eql('OK', HttpStatus['200']);
-	}
-};
