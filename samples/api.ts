db7c73e4f47ab44755fedf2477be66bcd41d4bac
David Worms <david@adaltas.com>
2024-10-17 22:18:24 +0200

    docs: covert api sample to ts

--- samples/api.ts
+++ samples/api.ts
@@ -1,4 +1,7 @@
-const status = require('..');
+// node --import tsx samples/api.js
+// node --loader ts-node/esm samples/api.ts
+import status from "../src/index.js";
+// import status from "http-status";
 
 console.info(status.INTERNAL_SERVER_ERROR);
 // Output: 500
@@ -7,14 +10,14 @@ console.info(status[500]);
 console.info(status[status.INTERNAL_SERVER_ERROR]);
 // Both output: "Internal Server Error"
 
-console.info(status['500_NAME']);
+console.info(status["500_NAME"]);
 console.info(status[`${status.INTERNAL_SERVER_ERROR}_NAME`]);
 // Both output: "INTERNAL_SERVER_ERROR"
 
-console.info(status['500_MESSAGE']);
+console.info(status["500_MESSAGE"]);
 console.info(status[`${status.INTERNAL_SERVER_ERROR}_MESSAGE`]);
 // Both output: "A generic error message, given when an unexpected condition was encountered and no more specific message is suitable."
 
-console.info(status['500_CLASS']);
+console.info(status["500_CLASS"]);
 console.info(status[`${status.INTERNAL_SERVER_ERROR}_CLASS`]);
 // Both output: "5xx"
